84e56c662b048be7eaba5b7a30c639344fd4d8e9
jknvlvxs <julio619alves@gmail.com>
2019-06-28 13:18:10 -0300

    remove hash from route and add login paths

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { LocationStrategy, HashLocationStrategy } from '@angular/common';
+import { LocationStrategy, PathLocationStrategy } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
@@ -21,7 +21,6 @@ import { DefaultLayoutComponent } from './containers';
 import { P404Component } from './views/error/404.component';
 import { P500Component } from './views/error/500.component';
 import { LoginComponent } from './views/login/login.component';
-import { RegisterComponent } from './views/register/register.component';
 
 const APP_CONTAINERS = [
   DefaultLayoutComponent
@@ -43,6 +42,7 @@ import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { TabsModule } from 'ngx-bootstrap/tabs';
 import { ChartsModule } from 'ng2-charts';
 import { HttpClientModule } from '@angular/common/http';
+import { CallbackComponent } from './callback/callback.component';
 
 @NgModule({
   imports: [
@@ -78,11 +78,11 @@ import { HttpClientModule } from '@angular/common/http';
     P404Component,
     P500Component,
     LoginComponent,
-    RegisterComponent,
+    CallbackComponent,
   ],
   providers: [{
     provide: LocationStrategy,
-    useClass: HashLocationStrategy,
+    useClass: PathLocationStrategy,
   }],
   bootstrap: [ AppComponent ]
 })
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -7,7 +7,8 @@ import { DefaultLayoutComponent } from './containers';
 import { P404Component } from './views/error/404.component';
 import { P500Component } from './views/error/500.component';
 import { LoginComponent } from './views/login/login.component';
-import { RegisterComponent } from './views/register/register.component';
+import { CallbackComponent } from './callback/callback.component';
+import { AuthGuard } from './auth/auth.guard';
 
 export const routes: Routes = [
   // {
@@ -15,6 +16,20 @@ export const routes: Routes = [
   //   redirectTo: 'dashboard',
   //   pathMatch: 'full',
   // },
+  {
+    path: 'callback',
+    component: CallbackComponent
+  },
+  {
+    path: 'logout',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    canActivate: [AuthGuard],
+    children: []
+  },
   {
     path: '404',
     component: P404Component,
@@ -37,13 +52,6 @@ export const routes: Routes = [
     }
   },
   {
-    path: 'register',
-    component: RegisterComponent,
-    data: {
-      title: 'Register Page'
-    }
-  },
-  {
     path: '',
     component: DefaultLayoutComponent,
     data: {
@@ -51,57 +59,35 @@ export const routes: Routes = [
     },
     children: [
       {
-        path: 'base',
-        loadChildren: () => import('./views/base/base.module').then(m => m.BaseModule)
-      },
-      {
         path: 'usuario',
-        loadChildren: () => import('./views/user/user.module').then(m => m.UserModule)
+        loadChildren: () => import('./views/user/user.module').then(m => m.UserModule),
+        canActivate: [AuthGuard]
       },
       {
         path: 'professor',
-        loadChildren: () => import('./views/teacher/teacher.module').then(m => m.TeacherModule)
+        loadChildren: () => import('./views/teacher/teacher.module').then(m => m.TeacherModule),
+        canActivate: [AuthGuard]
       },
       {
         path: 'curso',
-        loadChildren: () => import('./views/course/course.module').then(m => m.CourseModule)
+        loadChildren: () => import('./views/course/course.module').then(m => m.CourseModule),
+        canActivate: [AuthGuard]
       },
       {
         path: 'estudante',
-        loadChildren: () => import('./views/student/student.module').then(m => m.StudentModule)
+        loadChildren: () => import('./views/student/student.module').then(m => m.StudentModule),
+        canActivate: [AuthGuard]
       },
       {
         path: 'graficos',
-        loadChildren: () => import('./views/graficos/graficos.module').then(m => m.GraficosModule)
-      },
-      {
-        path: 'buttons',
-        loadChildren: () => import('./views/buttons/buttons.module').then(m => m.ButtonsModule)
-      },
-      {
-        path: 'charts',
-        loadChildren: () => import('./views/chartjs/chartjs.module').then(m => m.ChartJSModule)
+        loadChildren: () => import('./views/graficos/graficos.module').then(m => m.GraficosModule),
+        canActivate: [AuthGuard]
       },
       {
         path: 'dashboard',
-        loadChildren: () => import('./views/dashboard/dashboard.module').then(m => m.DashboardModule)
-      },
-      {
-        path: 'icons',
-        loadChildren: () => import('./views/icons/icons.module').then(m => m.IconsModule)
-      },
-      {
-        path: 'notifications',
-        loadChildren: () => import('./views/notifications/notifications.module').then(m => m.NotificationsModule)
-      },
-      {
-        path: 'theme',
-        loadChildren: () => import('./views/theme/theme.module').then(m => m.ThemeModule)
-      },
-      {
-        path: 'widgets',
-        loadChildren: () => import('./views/widgets/widgets.module').then(m => m.WidgetsModule)
-      },
+        loadChildren: () => import('./views/dashboard/dashboard.module').then(m => m.DashboardModule),
+        canActivate: [AuthGuard]
+      }
       // {
       //   path: 'user',
       //   loadChildren: () => import('./views/widgets/widgets.module').then(m => m.WidgetsModule)
